42ce521433d899511c83b548cded68d4ec9217d3
ali123606 <ah0681988@gmail.com>
2024-12-14 10:48:34 +0500

    update for vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import {
   BrowserRouter as Router,
   Routes,
@@ -18,7 +19,7 @@ const App = () => {
           <Route path='/Frameworks/ToLib' element={<Tool_Lib />} />
         </Routes>
       </Router>
-
+      <SpeedInsights/>
     </>
   )
 }
