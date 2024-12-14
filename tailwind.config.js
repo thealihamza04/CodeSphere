71eaf4436fe83555cb32902b7013caf69131e2ac
ali123606 <ah0681988@gmail.com>
2024-12-14 11:05:31 +0500

    update for vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
-// tailwind.config.mjs
-import daisyUI from 'daisyui';
+import daisyui from 'daisyui';
 
+/** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [daisyUI],
+  plugins: [daisyui],
 };
