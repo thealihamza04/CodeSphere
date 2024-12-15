2547cc75dac7463022e0b38babb67d5504b4d618
ali123606 <ah0681988@gmail.com>
2024-12-15 21:04:17 +0500

    add bottom margin on language page

--- src/Components/Languages.jsx
+++ src/Components/Languages.jsx
@@ -12,7 +12,7 @@ const Languages = () => {
     const def = "A programming language is a formal set of instructions that allows developers to communicate with computers to create software applications, scripts, or other tools. It provides the syntax and semantics for writing code that can perform specific tasks, manipulate data, and control hardware. Examples of programming languages include Python, Java, C++, and JavaScript, each with its own features, use cases, and paradigms.";
 
     return (
-        <>
+        <div className='mb-5'>
             <div className="my-9 mx-4 md:mx-8 lg:mx-16 space-y-6">
                 <h1 className="text-center text-3xl sm:text-4xl md:text-5xl font-bold">Programming Languages</h1>
                 <div className="px-4 md:px-48">
@@ -31,7 +31,7 @@ const Languages = () => {
                     />
                 ))}
             </div>
-        </>
+        </div>
     );
 };
 
