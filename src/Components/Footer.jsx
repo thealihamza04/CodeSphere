76fc345edc3af854a8b719466ea73fa13e1310cc
Ali Hamza <ah0681988@gmail.com>
2025-05-12 10:11:42 +0500

    add feedback form

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,35 +1,94 @@
 import { Link } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { FaLinkedinIn } from "react-icons/fa";
 import { PiGithubLogoDuotone } from "react-icons/pi";
 import { SiGmail } from "react-icons/si";
+import axios from "axios";
 
 const Footer = () => {
+  const [isLoading, setLoading] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const [isError, setIsError] = useState(false);
+
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   }, []);
 
+  const sendFeedback = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const name =
+      document.getElementById("name").value != ""
+        ? "undefined"
+        : document.getElementById("name").value;
+    const message = document.getElementById("message").value;
+
+    try {
+      const response = await axios.post(
+        "https://feed-backs.vercel.app/response/add",
+        {
+          webapp: "codes-sphere",
+          senderName: name,
+          senderEmail: "undefined",
+          message: message,
+        },
+        { timeout: 4000 }
+      );
+
+      if (response.status == 200) {
+        document.getElementById("name").value = "";
+        document.getElementById("message").value = "";
+        setShowAlert(true);
+        setTimeout(() => {
+          setShowAlert(false);
+        }, 1500);
+      } else {
+        setIsError(true);
+        setTimeout(() => {
+          setShowAlert(false);
+        }, 1500);
+      }
+    } catch (error) {
+      console.log(error);
+      setIsError(true);
+      setTimeout(() => {
+        setShowAlert(false);
+      }, 1500);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <>
-      <footer className='footer bg-gray-200 text-base-content p-10'>
-        <aside>
+    <div className='bg-gray-200'>
+      <div>
+        <div
+          role='alert'
+          className={`alert ${
+            isError ? "alert-error" : "alert-success"
+          } fixed  right-4 w-80 z-50 ${
+            showAlert ? "bottom-4" : "-bottom-24"
+          } transition-all duration-500 ease-in-out`}
+        >
           <svg
-            width='50'
-            height='50'
-            viewBox='0 0 24 24'
             xmlns='http://www.w3.org/2000/svg'
-            fillRule='evenodd'
-            clipRule='evenodd'
-            className='fill-current'
+            className='h-6 w-6 shrink-0 stroke-current'
+            fill='none'
+            viewBox='0 0 24 24'
           >
-            <path d='M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z'></path>
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
+              d='M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z'
+            />
           </svg>
-          <p>
-            ACME Industries Ltd.
-            <br />
-            Providing reliable tech since 1992
-          </p>
-        </aside>
+          <span>
+            {isError ? "ops! Server down" : " Thanks for your feedback!"}
+          </span>
+        </div>
+      </div>
+      <footer className='footer  text-base-content p-10 container mx-auto'>
         <nav>
           <h6 className='footer-title'>Services</h6>
           <Link to={"/"} className='link link-hover '>
@@ -69,8 +128,47 @@ const Footer = () => {
             </a>
           </div>
         </nav>
+        <form onSubmit={sendFeedback}>
+          <h6 className='footer-title'>FeedBack</h6>
+          <fieldset className='w-92 space-y-2'>
+            <div className='flex flex-row gap-3'>
+              <input
+                id='name'
+                type='text'
+                placeholder='your Good Name'
+                className='input input-sm focus:outline-none input-bordered'
+              />
+              <button
+                type='submit'
+                className={`btn btn-sm join-item relative ${
+                  isLoading ? "btn-disabled" : ""
+                } `}
+              >
+                <span
+                  className={`${
+                    isLoading ? "opacity-100" : "opacity-0"
+                  } loading loading-infinity loading-md text-white absolute transition-all duration-500 ease-in-out`}
+                ></span>{" "}
+                <span
+                  className={`${
+                    isLoading ? "opacity-0" : "opacity-100"
+                  } transition-all duration-500 ease-in-out`}
+                >
+                  Send
+                </span>
+              </button>
+            </div>
+            <textarea
+              className='input input-lg focus:outline-none text-xs py-2 px-2 resize-none w-full '
+              rows={3}
+              id='message'
+              placeholder='share your thoughts *'
+              required={true}
+            ></textarea>
+          </fieldset>
+        </form>
       </footer>
-    </>
+    </div>
   );
 };
 
